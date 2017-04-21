c719267adef04c314a85335084ee6ef69f22b135
Michael Ries <michael.ries@gmail.com>
2017-04-21 15:39:54 +0000

    add figuresController, index, and show

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,7 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
+    @figures = Figure.all
     erb :'/figures/index'
   end
 
@@ -9,18 +10,24 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    redirect to "owners/#{@figure.id}"
+    @figure = Figure.new(params[:figure])
+    @figure.save
+    redirect to "figures/#{@figure.id}"
   end
 
   get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
     erb :'/figures/edit'
   end
 
   get '/figures/:id' do
+    @figure = Figure.find(params[:id])
     erb :'/figures/show'
   end
 
   post '/figures/:id' do
-    redirect to "figures/#{@figure.id}"
+    figure=Figure.find(params[:id])
+    figure.update(params[:figure])
+    redirect to "figures/#{figure.id}"
   end
 end
